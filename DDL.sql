50893937163a461746932a3da3330da3de71ab51
jennaarowan <jennagirl001@gmail.com>
2022-05-11 13:17:09 -0400

    Initial repo creation and sql addition

---
+++ DDL.sql
@@ -0,0 +1,347 @@
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT = 0;
+
+-- -----------------------------------------------------
+-- Table Employees
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS Employees;
+
+CREATE TABLE IF NOT EXISTS Employees (
+  employeeID INT NOT NULL AUTO_INCREMENT,
+  socialSecurityNumber INT NOT NULL,
+  firstName VARCHAR(45) NOT NULL,
+  lastName VARCHAR(45) NOT NULL,
+  phoneNumber INT NOT NULL,
+  addressStreet VARCHAR(100) NOT NULL,
+  addressCity VARCHAR(45) NOT NULL,
+  addressState VARCHAR(45) NOT NULL,
+  addressZip INT NOT NULL,
+  PRIMARY KEY (employeeID))
+ENGINE = InnoDB;
+
+INSERT INTO Employees (
+  socialSecurityNumber,
+  firstName,
+  lastName,
+  phoneNumber,
+  addressStreet,
+  addressCity,
+  addressState,
+  addressZip
+) VALUES (
+  111223333,
+  "Hank",
+  "Schrader",
+  5016204338,
+  "4901 Cumbre Del Sur Court",
+  "Albuquerque",
+  "NM",
+  87106
+),
+(
+  222334444,
+  "Walter",
+  "White",
+  5051930809,
+  "308 Negra Arroyo Lane",
+  "Albuquerque",
+  "NM",
+  87111
+),
+(
+  333445555,
+  "Jesse",
+  "Pinkman",
+  5051483369,
+  "9809 Margo St",
+  "Albuquerque",
+  "NM",
+  87104
+);
+
+
+-- -----------------------------------------------------
+-- Table Stores
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS Stores;
+
+CREATE TABLE IF NOT EXISTS Stores (
+  storeID INT NOT NULL AUTO_INCREMENT,
+  addressStreet VARCHAR(100) NOT NULL,
+  addressCity VARCHAR(45) NOT NULL,
+  addressState VARCHAR(45) NOT NULL,
+  addressZip INT NOT NULL,
+  PRIMARY KEY (storeID))
+ENGINE = InnoDB;
+
+INSERT INTO Stores (
+  addressStreet,
+  addressCity,
+  addressState,
+  addressZip
+) VALUES (
+  "9516 Snow Heights Cir NE",
+  "Albuquerque",
+  "NM",
+  87112
+),
+(
+  "322 16th St SW",
+  "Albuquerque",
+  "NM",
+  87104
+),
+(
+  "2660 Fritts Crossing SE",
+  "Albuquerque",
+  "NM",
+  87105
+);
+
+
+-- -----------------------------------------------------
+-- Table RewardsTiers
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS RewardsTiers;
+
+CREATE TABLE IF NOT EXISTS RewardsTiers (
+  rewardsTierId INT NOT NULL AUTO_INCREMENT,
+  rewardsTierName VARCHAR(45) NOT NULL DEFAULT 'Bronze',
+  rewardsTierDiscount DECIMAL(10,2) NOT NULL DEFAULT .995,
+  rewardsTierMinPurchase INT NOT NULL,
+  PRIMARY KEY (rewardsTierId))
+ENGINE = InnoDB;
+
+INSERT INTO RewardsTiers(
+  rewardsTierName,
+  rewardsTierDiscount,
+  rewardsTierMinPurchase
+) VALUES (
+  "Bronze",
+  .995,
+  0
+),
+(
+  "Silver",
+  .9875,
+  500
+),
+(
+  "Gold",
+  .98,
+  1000
+);
+
+-- -----------------------------------------------------
+-- Table Customers
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS Customers;
+
+CREATE TABLE IF NOT EXISTS Customers (
+  customerID INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(45) NOT NULL,
+  firstName VARCHAR(45) NOT NULL,
+  lastName VARCHAR(45) NOT NULL,
+  addressStreet VARCHAR(100) NOT NULL,
+  addressCity VARCHAR(45) NOT NULL,
+  addressState VARCHAR(45) NOT NULL,
+  addressZip INT NOT NULL,
+  cusTotalSales INT NOT NULL DEFAULT 0,
+  RewardsTiers_rewardsTierId INT NOT NULL,
+  PRIMARY KEY (customerID),
+  INDEX fk_Customers_RewardsTiers1_idx (RewardsTiers_rewardsTierId ASC) VISIBLE,
+  CONSTRAINT fk_Customers_RewardsTiers1
+    FOREIGN KEY (RewardsTiers_rewardsTierId)
+    REFERENCES RewardsTiers (rewardsTierId)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+INSERT INTO Customers (
+  email,
+  firstName,
+  lastName,
+  addressStreet,
+  addressCity,
+  addressState,
+  addressZip,
+  cusTotalSales,
+  RewardsTiers_rewardsTierId
+) VALUES (
+  "gaslover@aol.com",
+  "Hank",
+  "Hill",
+  "72 Desert Rose Lane",
+  "Arlen",
+  "TX",
+  73104,
+  3450,
+  3
+),
+(
+  "watchoutfornsa@protonmail.com",
+  "Dale",
+  "Gribble",
+  "63 Desert Rose Lane",
+  "Arlen",
+  "TX",
+  73104,
+  7288.37,
+  3
+),
+(
+  "lonelybill@gmail.com",
+  "Bill",
+  "Dauterive",
+  "68 Desert Rose Lane",
+  "Arlen",
+  "TX",
+  73104,
+  867.21,
+  2
+);
+
+
+-- -----------------------------------------------------
+-- Table Orders
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS Orders;
+
+CREATE TABLE IF NOT EXISTS Orders (
+  orderID INT NOT NULL AUTO_INCREMENT,
+  Employees_employeeID INT NOT NULL,
+  Stores_storeID INT NOT NULL,
+  Customers_customerID INT NOT NULL,
+  orderTotal DECIMAL(10, 2) NOT NULL DEFAULT 0,
+  PRIMARY KEY (orderID),
+  INDEX fk_Orders_Employees_idx (Employees_employeeID ASC) VISIBLE,
+  INDEX fk_Orders_Stores1_idx (Stores_storeID ASC) VISIBLE,
+  INDEX fk_Orders_Customers1_idx (Customers_customerID ASC) VISIBLE,
+  CONSTRAINT fk_Orders_Employees
+    FOREIGN KEY (Employees_employeeID)
+    REFERENCES Employees (employeeID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_Orders_Stores1
+    FOREIGN KEY (Stores_storeID)
+    REFERENCES Stores (storeID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_Orders_Customers1
+    FOREIGN KEY (Customers_customerID)
+    REFERENCES Customers (customerID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+INSERT INTO Orders(
+  Employees_employeeID,
+  Stores_storeID,
+  Customers_customerID,
+  orderTotal
+) VALUES (
+  1,
+  1,
+  1,
+  39.18
+),
+(
+  2,
+  2,
+  2,
+  109.72
+),
+(
+  3,
+  3,
+  3,
+  12.93
+);
+
+
+-- -----------------------------------------------------
+-- Table Liquors
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS Liquors;
+
+CREATE TABLE IF NOT EXISTS Liquors (
+  productID INT NOT NULL AUTO_INCREMENT,
+  productName VARCHAR(100) NOT NULL,
+  productSizeMl INT NOT NULL DEFAULT 0,
+  productPrice DECIMAL(10,2) NULL,
+  PRIMARY KEY (productID))
+ENGINE = InnoDB;
+
+INSERT INTO Liquors(
+  productName,
+  productSizeMl,
+  productPrice
+) VALUES (
+  "100 Stories Gold Rush",
+  750,
+  19.99
+),
+(
+  "1792 Small Batch",
+  750,
+  27.99
+),
+(
+  "360 Vodka",
+  1000,
+  12.99
+);
+
+-- -----------------------------------------------------
+-- Table LiquorsOrders
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS LiquorsOrders;
+
+CREATE TABLE IF NOT EXISTS LiquorsOrders (
+  Liquors_productID INT NOT NULL,
+  Orders_orderID INT NOT NULL,
+  productQuantity INT NULL,
+  PRIMARY KEY (Liquors_productID, Orders_orderID),
+  INDEX fk_Orders_has_Liquors_Liquors1_idx (Liquors_productID ASC) VISIBLE,
+  INDEX fk_LiquorsOrders_Orders1_idx (Orders_orderID ASC) VISIBLE,
+  CONSTRAINT fk_Orders_has_Liquors_Liquors1
+    FOREIGN KEY (Liquors_productID)
+    REFERENCES Liquors (productID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_LiquorsOrders_Orders1
+    FOREIGN KEY (Orders_orderID)
+    REFERENCES Orders (orderID)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+INSERT INTO LiquorsOrders(
+  Liquors_productID,
+  Orders_orderID,
+  productQuantity
+) VALUES (
+  1,
+  1,
+  2
+),
+(
+  2,
+  2,
+  4
+),
+(
+  3,
+  3,
+  1
+);
+
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
---
+++ DML.sql
@@ -0,0 +1,132 @@
+/*
+Draft Version of DML queries for JL Liquor
+Author: Liam Maloney
+Date: May 5th, 2022
+*/
+
+-----------------Orders-------------------
+
+-- Create
+INSERT INTO Orders(orderID, employeeID, storeID, customerID, orderTotal)
+VALUES(:orderID_input, :employyeID_input, :storeID_input, :customerID_input, orderTotal);
+
+-- Read
+SELECT * FROM Orders
+
+-- Update
+-- Covered by cascade..?
+
+-- Delete
+-- Covered by cascade..?
+
+-----------------Stores-------------------
+-- Create
+INSERT INTO Stores(storeID, addressStreet, addressCity, addressState, addressZip)
+VALUES(:storeID_input, :addressStreet_input, :addressCity_input, :addressState_input, :addressZip_input);
+
+-- Read
+SELECT * FROM Stores
+
+-- Update
+UPDATE Stores SET
+    addressStreet = :addressStreet_input, addressCity = :addressCity_input, addressState = :addressState_input, 
+    addressZip = :addressZip
+    WHERE storeID = :storeID_input
+
+-- Delete
+DELETE FROM Stores WHERE Stores.storeID = :storeID_input;
+
+
+-----------------Customers-------------------
+-- Create
+INSERT INTO Customers(customerID, email, firstName, lastName, addressStreet, addressCity, addressState, addressZip, totalSales, rewardsTierId)
+VALUES(:customerID_input, :email_input, :firstName_input, :lastName_input, :addressStreet_input, :addressCity_input, :addressState_input, :addressZip_input, :totalSales_input, :rewardsTierId_input);
+
+-- Read
+SELECT * FROM Customers
+
+-- Update
+UPDATE Customers SET
+    customerID = :customerID_input, email = :email_input, first_name = :firstName_input, lastname = :lastName_input, 
+    addressStreet = :addressStreet_input, addressCity = :addressCity_input, addressState = :addressState_input, addressZip = :addressZip_input, totalSales = :totalSales_input, rewardsTierId = :rewardsTierId_input
+    WHERE customerID = :customerID_input
+
+-- Delete
+DELETE FROM Customers WHERE Customers.customerId = :customerID_input;
+
+
+
+-----------------Employees-------------------
+-- Create
+INSERT INTO Employees(employeeID, SocialSecurityNumber, firstName, lastName, phoneNumber, addressStreet, addressCity, addressState, addressZip)
+VALUES(:employeeID_input, :SocialSecurityNumber, :firstName_input, :lastName_input, :phoneNumber_input, :addressStreet_input, :addressCity_input, :addressState_input, :addressZip_input);
+
+-- Read
+SELECT * FROM Employees
+
+-- Update
+UPDATE Employees SET
+    employeeID = :employeeID_input, SocialSecurityNumber = :SocialSecurityNumber, first_name = :firstName_input, lastname = :lastName_input, 
+    addressStreet = :addressStreet_input, addressCity = :addressCity_input, addressState = :addressState_input, addressZip = :addressZip_input
+    WHERE employeeID = :employeeID_input
+
+-- Delete
+DELETE FROM Employees WHERE Employees.employeeId = :employeeID_input;
+
+
+
+-----------------Liquors-------------------
+-- Create
+INSERT INTO Liquors(productID, productName, productSize, productPrice)
+VALUES(:productID_input, :productName_input, :productSize_input, :productPrice_input);
+
+-- Read
+SELECT * FROM Liquors
+
+-- Update
+UPDATE Liquors SET
+    productID = :productID_input, productName = :productName_input, productSize = :productSize_input, productPrice = :productPrice_input
+    WHERE productID = :productID_input
+
+-- Delete
+DELETE FROM Liquors WHERE Liquors.productID = :prodcutID_input;
+
+
+
+-----------------RewardsTiers-------------------
+
+-- Create
+INSERT INTO RewardsTiers(rewardsTierID, rewardsTierName, rewardsTierDiscount, rewardsTierMinPurchase)
+VALUES(:rewardsTierID_input, :rewardsTierName_input, :rewardsTierDiscount_input, :rewardsTierMinPurchase_input);
+
+-- Read
+SELECT * FROM RewardsTiers
+
+-- Update
+UPDATE RewardsTiers SET
+    produrewardsTierID = :rewardsTierID_input, rewardsTierName = :rewardsTierName_input, rewardsTierDiscount = :rewardsTierDiscount_input, rewardsTierMinPurchase = :rewardsTierMinPurchase_input
+    WHERE RewardsTiersID = :RewardsTiersID_input
+
+-- Delete
+DELETE FROM RewardsTiers WHERE RewardsTiers.RewardsTiersID = :RewardsTiersID_input;
+
+
+
+-------------------LiquorsOrders-------------------
+
+-- Create
+INSERT INTO LiquorsOrders(productID, OrderID, productQuantity)
+VALUES(:productID_input, :OrderID_input, :productQuantity_input);
+
+-- Read
+SELECT * FROM LiquorsOrders
+
+-- Update
+-- Change this to cascade?
+UPDATE LiquorsOrders SET
+    productID = :productID, OrderID = :OrderID, productQuantity = :productQuantity_input
+    WHERE productID = :productID_input and orderID = :orderID_input
+
+
+-- Delete
+DELETE FROM LiquorsOrders WHERE LiquorsOrders.LiquorsOrdersID = :LiquorsOrdersID_input;
